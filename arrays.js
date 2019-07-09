a246fb9eae98f021052a26bd6676d0e304cd7aa3
Katrina R <katrina.reinsdorff@gmail.com>
2019-07-09 20:41:23 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -40,7 +40,8 @@ function destructivelyRemoveElementFromBeginningOfArray (array){
 }
 
 function removeElementFromBeginningOfArray(array){
-  array.slice();
+  array.slice(1);
+  array = array.slice(1);
   return array
 }
 
