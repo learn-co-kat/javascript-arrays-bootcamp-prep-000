33ce1f34aba11758ffbf33a556ed42018ac2f065
Katrina R <katrina.reinsdorff@gmail.com>
2019-07-09 20:05:12 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -19,53 +19,37 @@ function destructivelyAddElementToBeginningOfArray(array, element) {
   return array;
 }
 
-destructivelyAddElementToBeginningOfArray([1], 'foo');
-
 function addElementToEndOfArray(array,element){
   [element, ...array];
   array = [element, ...array];
   return array;
 }
 
-addElementToEndOfArray([1], 'foo');
-
 function destructivelyAddElementToEndOfArray(array, element){
   array.push(element);
   return array;
 }
 
-destructivelyAddElementToEndOfArray([1], 'foo');
-
 function accessElementInArray(array, index) {
   return(array[2]);
 }
-
-accessElementInArray([1, 2, 3], 2);
 
 function destructivelyRemoveElementFromBeginningOfArray (array){
   array.shift();
   return array;
 }
 
-destructivelyRemoveElementFromBeginningOfArray([1,2,3]);
-
 function removeElementFromBeginningOfArray(array){
   array.slice(1);
 }
-
-removeElementFromBeginningOfArray([1,2,3]);
 
 function destructivelyRemoveElementFromEndOfArray(array){
   array.pop();
 }
 
-destructivelyRemoveElementFromEndOfArray([1,2,3]);
-
 function removeElementFromEndOfArray(array){
   array.slice(0, array.length - 1);
 }
-
-removeElementFromEndOfArray([1,2,3]);
 
 
 
