c1fbd401c2f60b5f897d80e8396bdda576fa701a
Katrina R <katrina.reinsdorff@gmail.com>
2019-07-09 20:32:33 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -41,12 +41,15 @@ function destructivelyRemoveElementFromBeginningOfArray (array){
 
 function removeElementFromBeginningOfArray(array){
   array.slice(1)
+  return array
 }
 
 function destructivelyRemoveElementFromEndOfArray(array){
   array.pop()
+  return array
 }
 
 function removeElementFromEndOfArray(array){
   array.slice(0, array.length - 1);
+  return array
 }
